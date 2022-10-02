5293239b6a8f13e6ba23fad6ad5a869023b5bc01
tenoclock <enr@bk.ru>
2022-10-03 00:08:34 +0500

    common header

---
+++ src/components/Header.js
@@ -0,0 +1,260 @@
+import React from "react";
+import $ from "jquery";
+import { WOW } from "../assets/js/wow.js";
+
+import "../assets/js/tmstickup.js";
+import "../assets/js/jquery.ui.totop.my.js";
+import "../assets/js/parallax.js";
+import "../assets/js/jquery.easing.1.3.js";
+
+import i from "../assets/images/logo-inverse-162x54.png";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    new WOW().init();
+    $("button").click(function() {
+      $(".rd-navbar-toggle").toggleClass("active");
+      $(".rd-navbar-nav-wrap").toggleClass("active");
+    });
+    $(".rd-navbar").TMStickUp({});
+    $().UItoTop({
+      easingType: "easeOutQuad",
+      containerClass: "ui-to-top fa fa-angle-up",
+    });
+  }
+  render() {
+    return (
+      <header className="section page-header">
+        {}
+        <div className="rd-navbar-wrap">
+          <nav className="rd-navbar rd-navbar-classic rd-navbar-original rd-navbar-static">
+            <div className="rd-navbar-main-outer">
+              <div className="rd-navbar-main">
+                {}
+                <div className="rd-navbar-panel">
+                  {}
+                  <button
+                    className="rd-navbar-toggle"
+                    data-rd-navbar-toggle=".rd-navbar-nav-wrap"
+                  >
+                    <span />
+                  </button>
+                  {}
+                  <div className="rd-navbar-brand">
+                    <a href="index.html">
+                      <img
+                        className="brand-logo-light"
+                        src={i}
+                        alt
+                        width={162}
+                        height={54}
+                        srcSet="images/logo-inverse-324x108.png 2x"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="rd-navbar-main-element">
+                  <div className="rd-navbar-nav-wrap">
+                    {}
+                    <ul className="rd-navbar-nav">
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="index.html">
+                          Home
+                        </a>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="about-us.html">
+                          About Us
+                        </a>
+                        {}
+                        <ul className="rd-menu rd-navbar-dropdown">
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="our-team.html"
+                            >
+                              Our Team
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="team-member.html"
+                            >
+                              Team Member
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="pricing.html">
+                          Pricing
+                        </a>
+                      </li>
+                      <li className="rd-nav-item active">
+                        <a className="rd-nav-link" href="news.html">
+                          News
+                        </a>
+                        {}
+                        <ul className="rd-menu rd-navbar-dropdown">
+                          <li className="rd-dropdown-item">
+                            <a className="rd-dropdown-link" href="news-2.html">
+                              News 2
+                            </a>
+                          </li>
+                          <li className="rd-dropdown-item">
+                            <a
+                              className="rd-dropdown-link"
+                              href="blog-post.html"
+                            >
+                              Blog Post
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="#">
+                          Pages
+                        </a>
+                        {}
+                        <ul className="rd-menu rd-navbar-megamenu">
+                          <li className="rd-megamenu-item">
+                            <h6 className="rd-megamenu-title">Pages 1</h6>
+                            <ul className="rd-megamenu-list">
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="typography.html"
+                                >
+                                  Typography
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="buttons.html"
+                                >
+                                  Buttons
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="timers-&-counters.html"
+                                >
+                                  Timers & Counters
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="forms.html"
+                                >
+                                  Forms
+                                </a>
+                              </li>
+                            </ul>
+                          </li>
+                          <li className="rd-megamenu-item">
+                            <h6 className="rd-megamenu-title">Pages 2</h6>
+                            <ul className="rd-megamenu-list">
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="grid-system.html"
+                                >
+                                  Grid system
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="icon-lists.html"
+                                >
+                                  Icon Lists
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="privacy-policy.html"
+                                >
+                                  Privacy policy
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="coming-soon.html"
+                                >
+                                  Coming Soon
+                                </a>
+                              </li>
+                            </ul>
+                          </li>
+                          <li className="rd-megamenu-item">
+                            <h6 className="rd-megamenu-title">Pages 3</h6>
+                            <ul className="rd-megamenu-list">
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="awards.html"
+                                >
+                                  Awards
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="gallery-1.html"
+                                >
+                                  Gallery 1
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="gallery-2.html"
+                                >
+                                  Gallery 2
+                                </a>
+                              </li>
+                              <li className="rd-megamenu-list-item">
+                                <a
+                                  className="rd-megamenu-list-link"
+                                  href="search-results.html"
+                                >
+                                  Search Results
+                                </a>
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="rd-nav-item">
+                        <a className="rd-nav-link" href="contacts.html">
+                          Contacts
+                        </a>
+                      </li>
+                    </ul>
+                    <a className="button button-primary button-sm" href="#">
+                      book now
+                    </a>
+                  </div>
+                </div>
+                <a className="button button-primary button-sm" href="#">
+                  book now
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
