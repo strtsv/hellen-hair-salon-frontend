62098964f335e420c0f74d1af82a778f0d9d6ae5
tenoclock <enr@bk.ru>
2023-12-26 13:04:37 +0500

    finish

--- src/App.js
+++ src/App.js
@@ -5,27 +5,15 @@ import "./assets/css/bootstrap.css";
 import "./assets/css/fonts.css";
 import "./assets/css/style.css";
 
-import Home from "./pages/Home";
-import Team from "./pages/Team";
-// import Gallery from "./pages/Gallery";
-// import Team from "./pages/Team";
-import GridBlog from "./pages/GridBlog";
-import Careers from "./pages/Careers";
-// import NewsPost from "./pages/NewsPost";
-// import Testimonials from "./pages/Testimonials";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/index.html" component={Home} />
-      <Route exact path="/Team.html" component={Team} />
-      <Route exact path="/grid-blog.html" component={GridBlog} />
-      {/* <Route exact path="/our-team.html" component={Team} /> */}
-      {/* <Route exact path="/gallery.html" component={Gallery} /> */}
-      {/* <Route exact path="/testimonials.html" component={Testimonials} /> */}
-      {/* <Route exact path="/news-post.html" component={NewsPost} /> */}
-      <Route exact path="/careers.html" component={Careers} />
-      <Route exact path="/" component={Home} />
+      <Route exact path="/blog-post.html" component={BlogPost} />
+      <Route exact path="/index.html" component={Blog} />
+      <Route exact path="/" component={Blog} />
     </Switch>
   );
 }
